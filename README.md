879b897e6607b1830ec0345cf31f1ccb11830c46
Atikhun Orisoon <genatorster@gmail.com>
2016-04-17 00:32:21 +0700

    Add Pug prototype,  more working on Express, edit README file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# messenger
+# Iris Messenger
 Realtime instant messaging service
+
+## Why Iris
+Iris is well-known as the goddess of messenger in Greek mythology.
+
+## Features
+* Group chat
--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var port = 8081;
+
+var server = app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+if (app.get('env') == 'development') {
+    app.locals.pretty = true;
+}
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
